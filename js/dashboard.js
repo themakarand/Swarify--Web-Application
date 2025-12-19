9b1d6ff201d4922d70802111bdb1ad7c01643b72
Makarand Upadhye <makarandupadhye07@gmail.com>
2025-12-19 18:48:17 +0530

    Refactor song list and add new song 'Chammak Challo'

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,7 +4,7 @@ const songs = [
  {name:"Sharat",artist:"jasmine sandlas",img:"covers/cover50.jpg",src:"songs/Shararat Dhurandhar 128 Kbps.mp3"},
  { name: "Wavy", artist: "Karan Aujla", img: "covers/cover4.jpg", src: "songs/song12.mp3" },
  { name:"Dhurandhar Title Track", artist: "Karan Aujla", img: "covers/cover5.jpg", src: "songs/Title Track Dhurandhar 128 Kbps.mp3" },
- 
+  { name: "Chammak Challo", artist: "Akon", img: "covers/cover51.jpg", src: "songs/Chammak Challo Ra One 128 Kbps.mp3" },
  { name: "Winning Speech", artist: "Karan Aujla", img: "covers/cover10.jpg", src: "songs/song10.mp3" },
  { name: "One Love", artist: "Shubh", img: "covers/cover11.jpg", src: "songs/One Love Shubh 128 Kbps.mp3" },
  {name :"Laila main Laila",artist:"Pawni Pandey", img:"covers/cover6.jpg", src:"songs/Laila Main Laila.mp3"},
@@ -75,4 +75,5 @@ searchInput.addEventListener("input", () => {
 });
 
 loadSongs();
+
 
